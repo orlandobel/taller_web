0fa28d3ad6dbb91c17e019bf4da3ea6e789036c3
orlandobel <orlando.bel.jimmy@gmail.com>
2020-10-12 12:34:23 -0500

    Formularios p1

---
+++ sesion4/destino.php
@@ -0,0 +1,11 @@
+<?php
+    echo $_POST['usuario'].'<br>';
+    echo $_POST['ap_paterno'].'<br>';
+    echo $_POST['email'].'<br>';
+    echo $_POST['pass'].'<br>';
+    echo $_POST['deportes'][0].'<br>';
+    echo $_POST['deportes'][1].'<br>';
+    echo $_POST['deportes'][2].'<br>';
+    echo $_POST['genero'].'<br>';
+    echo $_POST['ciudad'];
+?>
