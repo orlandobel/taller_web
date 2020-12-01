ec82f3717f2a64cdb983ae716b98e02e0a642efe
KevBS <kevb612@gmail.com>
2020-11-30 22:07:30 -0600

    Matenme

---
+++ Proyecto/views/Lista.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylo.css">
+    <link rel="stylesheet" href="../Templates/css/stylo.css">
+    <title>Document</title>
+
+
+    
+</head>
+
+
+
+<div class="header">
+    <div id="title">
+        <label>P o l i s t o r e</label>
+    </div>
+   
+
+<br>
+    <div id="Menu">
+        <ul class="nav">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Departamentos</a>
+                <ul>
+                    <li><a href="">Hogar</a></li>
+                    <li><a href="">Computo y electrónica</a></li>
+                    <li><a href="">Videojuegos</a></li>
+                    <li><a href="">Celulares</a></li>
+                    <li><a href="">Línea blanca</a>
+                        <ul>
+                            <li><a href="">Submenu1</a></li>
+                            <li><a href="">Submenu2</a></li>
+                            <li><a href="">Submenu3</a></li>
+                            <li><a href="">Submenu4</a></li>
+                            <li><a href="">Submenu5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+
+
+
+    <div class="search_bar">
+        <input type="text" class="search" placeholder="Search">
+        <button>
+            <i class="material-icons">search</i>
+        </button>
+    </div>
+
+    
+    
+
+<body>
+    <?php include_once 'layouts/headers.html'; ?>
+ 
+  
+    
+
+
+</body>
+
+
+</html>
+
